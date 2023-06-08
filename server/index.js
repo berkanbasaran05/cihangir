5cbceb5c419be872b785b17d61ea1ab3e3dd5e85
berkanbasaran05 <77754197+berkanbasaran05@users.noreply.github.com>
2023-06-08 05:05:54 +0300

    last

--- server/index.js
+++ server/index.js
@@ -13,9 +13,13 @@ app.use(express.json({ limit: '50mb' }));
 
 
 
-app.use('/', examResultRouter);
+app.use('/lgs-sonuc', examResultRouter);
 
-
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
 
 
 
